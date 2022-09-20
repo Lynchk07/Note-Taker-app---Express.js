5d1eafa19de5c5fb9d7de449acbdef43fbca8567
Lynchk07 <lynch.kristinj@gmail.com>
2022-09-19 22:04:25 -0700

    updated fs.writeFile

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const fs =require('fs');
 
 //app variable for express - this is the express app - server
 const app = express();
-//port to run the server 
-const noteData = require('./Develop/db/db.json');
-const PORT = 3001;
 
-//use of middleware-serve html files. all HTMLs file to send to your users browsers. Use public directory.
+//port to run the server 
+const PORT = process.env.PORT ||3001;
+
+//use of middleware- to serve html files. all HTMLs file to send to your users browsers. Use public directory.
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static('public'));
+let notes = require('./Develop/db/db.json');
+
+
 //get request to parse json
 //uses get method to fetch the root pathway - get route for static homepage
 
@@ -23,12 +29,19 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-//
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+//log notes 
+app.get('/api/notes',(req,res) => {
+  fs.readFile('db/db.json','utf8', (err,data) =>{
+    if(err) {
+      console.log (err);
+      return;
+    }
+    res.json(notes);
+  });
+})
 
-app.get('/api/notes', (req, res) => {
+//Api get
+app.get('/api/notes/:id', (req, res) => {
   res.json({
     term: 'api',
     description:
@@ -36,30 +49,37 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
-app.get('/api', (req, res) => {
-  res.json({
-    term: 'api',
-    description:
-      'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
-  });
+//res.json() allows us to return JSON instead of a buffer. 
+app.get('/api/public/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf-8',(err,data) => {
+    if(err) {
+      console.error(err);
+    } else{
+      //Convert string to JSON object
+    const notes = JSON.parse(data);
+
+      //Add a new review
+      notes.push(newNote);
+
+      //write back to the file
+      fs.writeFile('./db/db.json',JSON.stringify(notes,null))
+    
+    };
 });
-//res.json() allowa us to return JSON instead of a buffer. 
-app.get('/api/public/:notes', (req, res) => res.json(noteData));
 
-  const noteTracker = req.params.notes.delete();
 
-for (let i = 0; i < noteData.length; i++) {
-  const element = array[i];
-  
-}
+// //delete notes 
+// app.delete('/api/notes/:deleteNote_id',(req,res)=> {
+// res.deleteFile(path.join(_dirname,'public/notes.html'));
+// });
 
+
+// Catch all error code 
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(_dirname,'pubic/indexedDB,html'));
+});
+
+//Starts server to begin listening
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
-
-// app.delete('/api/notes/:deleteNote_id',(req,res)=>
-// res.deleteFile(path.join(_dirname,'public/notes.html'))
-
-// )
-
-
